ee5097e6716bec5b6412b12c59d08f44b45e28b9
Akhil Mittal <mittalakhil.1992@gmail.com>
2020-05-07 23:43:22 +0530

    added another user and wrong user as well

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
-INSERT INTO users(id, name, email, password, is_logged_in) VALUES(1, 'abc', 'abc@gmail.com', 'abc@123', 0);
+INSERT INTO users(name, email, password, is_logged_in) 
+VALUES('abc', 'abcd@gmail.com', 'abc@123', 'abcd-123-shbbchsj-12bbbch', 0), 
+('abc', 'abc@gmail.com', 'abcd123456', 'abcd-12345-shbbchsj-12bbbch', 0);
