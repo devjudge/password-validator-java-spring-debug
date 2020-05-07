859096b33a46e70ae2693ff00f6280649455bd39
Akhil Mittal <mittalakhil.1992@gmail.com>
2020-05-07 23:08:50 +0530

    updated password

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO users(id, name, email, password, is_logged_in) VALUES(1, 'abc', 'abc@gmail.com', 'abcd123456', 0);
+INSERT INTO users(id, name, email, password, is_logged_in) VALUES(1, 'abc', 'abc@gmail.com', 'abc@123', 0);
