1a978fd2469a671f46688abf19860cf4fa6e6d0c
Akhil Mittal <mittalakhil.1992@gmail.com>
2020-05-07 23:54:46 +0530

    added auth token

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO users(name, email, password, is_logged_in) 
+INSERT INTO users(name, email, password, auth_token, is_logged_in) 
 VALUES('abc', 'abcd@gmail.com', 'abc@123', 'abcd-123-shbbchsj-12bbbch', 0), 
 ('abc', 'abc@gmail.com', 'abcd123456', 'abcd-12345-shbbchsj-12bbbch', 0);
